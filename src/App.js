7bf4757cba438e1f5e90505247a97d2dbabe3d7c
MarjanGit <mansar16@huntersoe.org>
2019-09-30 21:23:55 -0400

    base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio"
+import Contact from "./components/Contact"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                Portfolio
+        </div>);
+    }
+}
+
+export default Portfolio;
