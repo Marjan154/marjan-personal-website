85f8bf7acfcc56f0cce980ea705fd3ffa1754520
MarjanGit <mansar16@huntersoe.org>
2019-10-01 00:41:52 -0400

    link to blog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles/navbar.css"
+import styles from "../styles/navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="nav">
-            <ul>
-                <li>
-                    {" "}
-                    <Link to="/">Home</Link>{" "}
-                </li>
-                <li>
-                    {" "}
-                    <Link to="/About/">About</Link>{" "}
-                </li>
-                <li>
-                    {" "}
-                    <Link to="/Portfolio/">Portfolio</Link>{" "}
-                </li>
-                <li>
-                    {" "}
-                    <Link to="/Account/">Contact</Link>{" "}
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          {" "}
+          <Link to="/">Home</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link to="/About/">About</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link to="/Portfolio/">Portfolio</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link to="/Account/">Contact</Link>{" "}
+        </li>
+        <li>
+          <a href="https://ansarmarjan154.wixsite.com/website" target="_blank">
+            Blog
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
