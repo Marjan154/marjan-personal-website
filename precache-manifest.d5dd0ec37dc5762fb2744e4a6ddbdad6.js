af0cc93a83bf9c08509f2083d6a7353f8f370701
MarjanGit <mansar16@huntersoe.org>
2019-12-15 00:03:02 -0500

    Updates

--- precache-manifest.d5dd0ec37dc5762fb2744e4a6ddbdad6.js
+++ precache-manifest.d5dd0ec37dc5762fb2744e4a6ddbdad6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f80117e6d7adae727dfa2ee1eb84ce3",
+    "revision": "7ec3e745e3eccd42d4275063930b213c",
     "url": "/marjan-personal-website/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/marjan-personal-website/static/css/2.1fd2b905.chunk.css"
   },
   {
-    "revision": "98ef18932ea46d7850b2",
+    "revision": "03f58f57df1a364a40a0",
     "url": "/marjan-personal-website/static/css/main.7df47c31.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/marjan-personal-website/static/js/2.805d6f6f.chunk.js"
   },
   {
-    "revision": "98ef18932ea46d7850b2",
-    "url": "/marjan-personal-website/static/js/main.99cc4950.chunk.js"
+    "revision": "03f58f57df1a364a40a0",
+    "url": "/marjan-personal-website/static/js/main.66be70ca.chunk.js"
   },
   {
     "revision": "92814015ebbf7d181e73",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marjan-personal-website/precache-manifest.ff53d4e93d90d63d472807f2c360aa68.js"
+  "/marjan-personal-website/precache-manifest.d5dd0ec37dc5762fb2744e4a6ddbdad6.js"
 );
 
 self.addEventListener('message', (event) => {
