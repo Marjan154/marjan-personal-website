5e01a11f0d574f30683ccd3dd094d6df3ea172e2
MarjanGit <mansar16@huntersoe.org>
2019-12-14 21:57:46 -0500

    centered text

--- src/components/AnimateSection.js
+++ src/components/AnimateSection.js
@@ -1,17 +1,5 @@
 import React, { Component, useState, useRef, useEffect } from "react";
 
-const isInViewport = element => {
-  if (element) {
-    const rect = element.getBoundingClientRect();
-    const html = document.documentElement;
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || html.clientHeight) &&
-      rect.right <= (window.innerWidth || html.clientWidth)
-    );
-  }
-};
 const AnimateSection = props => {
   const [isVisible, setVisible] = React.useState(true);
   const domRef = React.useRef();
@@ -22,10 +10,6 @@ const AnimateSection = props => {
     observer.observe(domRef.current);
     return () => observer.unobserve(domRef.current);
   }, []);
-  const elem = document.getElementById("testing");
-  if (isInViewport(elem)) {
-    console.log("yes");
-  }
 
   return (
     <div
